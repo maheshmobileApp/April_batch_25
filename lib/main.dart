edd07477826935b189c6fb1c3cc2f825ad0717d6
mahesh <>
2025-04-30 08:26:30 +0530

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,37 @@
 import 'package:flutter/material.dart';
-
+import 'splash_screen.dart';
+import 'login_screen.dart';
 void main() {
-  runApp(const MyApp());
+  // MyFirstApp app = MyFirstApp();
+  runApp(MyFirstApp());
+}
+class MyFirstApp extends StatefulWidget {
+  const MyFirstApp({super.key});
+
+  @override
+  State<MyFirstApp> createState() => _MyFirstAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/*
+Statefull widget -> two parts
+1. immutable part
+2. state class (mutablePart)
+* */
 
-  // This widget is the root of your application.
+class _MyFirstAppState extends State<MyFirstApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:SplashScreen()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+//lib->main.dart->main -> runApp(widget) -> MyFirstApp -> Build -> MaterialApp() -> home -> Scaffold -> appBar,body
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
 /*
+
+widget: 1. statefull 2. stateless widget 
 
 Target -> physical device / emulator
 
@@ -135,6 +42,19 @@ Create Emulator ->
 
 In Android Studio -> Go to  Device Manger -> Click on (+) Create Virtual Device ->
 
+1. Container -> div
+2. Text
+3. Button
+4. TextField
+5. Image
+6. Row
+7. Column
+8.Listview
+9.ScrollView
+10.gridview
+11. padding
+12.margin
+13. wrap
 
 
 * */
---
+++ lib/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Splash"),),
+      body: Container(
+        child: Text("Hello this is Text widget"),
+        color: Colors.red,
+        width: 250,
+        height: 300,
+        margin: EdgeInsets.only(left: 10,right: 20,top: 30),
+      ),
+    );
+  }
+}
+/*
+Container: is like a Box that wraps its child widget and allows you to style and position it
+child : the widget inside the container(text,image, row, column......)
+width & height:
+
+
+* */
